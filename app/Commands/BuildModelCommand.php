4152d26bcf3529b4fac8c1781ccb97a7e8dd7c04
Yunus Emre Deligöz <ye@deligoz.me>
2021-10-29 02:04:46 +0300

    Create BuildModelCommand

---
+++ app/Commands/BuildModelCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\File;
+use LaravelZero\Framework\Commands\Command;
+use MessagePack\MessagePack;
+use Phonyland\LanguageModel\Model;
+use Phonyland\NGram\TokenizerFilter;
+
+class BuildModelCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'build
+                            {path : Path of the text file}
+                            {--name=Phony Language Model : A name for the model}
+                            {--n=3 : N-Gram}
+                            {--min-lenght=3 : The minimum length of the word considered in the generation of the model}
+                            {--unique=false : Avoid skewing the generation toward the most repeated words in the text corpus}
+                            {--exclude-originals=false : Blacklist original words from generation}
+                            {--frequency-precision=7 : Frequency Precision}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Feed, build, and save the model';
+
+    public function handle(): void
+    {
+        $model = new Model($this->option('name'));
+
+        $this->task((string) ($this->option('name')), function () use ($model) {
+            $model->config->n((int) $this->option('n'))
+                          ->minLenght((int) $this->option('min-lenght'))
+                          ->unique(!($this->option('unique') === 'false'))
+                          ->excludeOriginals(!($this->option('exclude-originals') === 'false'))
+                          ->frequencyPrecision((int) $this->option('frequency-precision'))
+                ->tokenizer->addWordSeparatorPattern(TokenizerFilter::WHITESPACE_SEPARATOR)
+                           ->addWordFilterRule(TokenizerFilter::LATIN_EXTENDED_ALPHABETICAL)
+                           ->addSentenceSeparatorPattern(['.', '?', '!', ':', ';', '\n'])
+                           ->toLowercase();
+        });
+
+        $this->task('Feeding', function () use ($model) {
+            $data = File::lines(getcwd().'/'.$this->argument('path'));
+
+            foreach ($data as $index => $line) {
+                $model->feed($line);
+            }
+        });
+
+        $this->task('Calculating', function () use ($model) {
+            $model->calculate();
+        });
+
+        $this->task('Encoding & Compressing', function () use ($model) {
+            File::put(getcwd().'/output.phony', gzencode(MessagePack::pack($model->build())));
+        });
+    }
+}
